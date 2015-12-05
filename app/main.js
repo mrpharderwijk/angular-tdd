a03f8db68b120314c9f6063882fc4233b8af445a
Marnix Harderwijk <m.harderwijk@littlebigagency.com>
2015-12-05 11:45:02 +0100

    3.3 Testing Angular Controllers

--- app/main.js
+++ app/main.js
@@ -1,8 +1,20 @@
 angular.module('AddressBook', [])
-  .service('contactService', function($http) {
-    this.contacts = [];
 
-    $http.get('http://localhost:9001/contacts', function(res) {
+.service('contactService', function($http) {
+  this.contacts = [];
+
+  var contactService = this
+
+  $http.get('http://localhost:9001/contacts')
+    .then(function(res) {
       console.log(res);
+
+      while(res.data[0]) {
+        contactService.contacts.push(res.data.pop());
+      }
     });
-  });
+})
+
+.controller('ContactController', function(contactService, $scope) {
+  $scope.contacts = contactService.contacts;
+});
