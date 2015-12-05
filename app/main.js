81847a40fadfeccb17bd03c9f91ed0d2e4bc86fa
Marnix Harderwijk <m.harderwijk@littlebigagency.com>
2015-12-05 11:25:10 +0100

    3.2 Verifying GET and POST Functionality with $httpBackend

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ angular.module('AddressBook', [])
   .service('contactService', function($http) {
     this.contacts = [];
 
-    $http.get('localhost:9001/contacts', function(res) {
+    $http.get('http://localhost:9001/contacts', function(res) {
       console.log(res);
     });
   });
