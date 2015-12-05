a6434a2604b909990529a8eb73ff89a77c9719bd
Marnix Harderwijk <m.harderwijk@littlebigagency.com>
2015-12-05 11:17:52 +0100

    3.1 Testing Services With Inject and Module

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
-angular.module('AddressBook', []);
+angular.module('AddressBook', [])
+  .service('contactService', function($http) {
+    this.contacts = [];
+
+    $http.get('localhost:9001/contacts', function(res) {
+      console.log(res);
+    });
+  });
